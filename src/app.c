4e48670de6f168ad4e41215e86ea3c5df3f0bf27
DevCam <josean.camarena@gmail.com>
2020-08-17 14:20:11 -0500

    HON can pull a random name from a given hat

--- src/app.c
+++ src/app.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "HatONames.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
   HON_hat hat;
   HatONames.InitializeHat(&hat, "./data/names");
+
+  HON_name person = HatONames.PullName(hat);
+  printf("%s was selected!\n", person.nickname);
   return 0;
 }
